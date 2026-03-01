3b85184c380a97e3d852863129342e65a90dc97f
hellstumy <e.one1604w@gmail.com>
2026-03-01 22:50:42 +0100

    add cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,12 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(cors())
+app.use(
+  cors({
+    origin: true, // разрешает любой домен
+    credentials: true,
+  })
+)
 app.use(express.json())
 
 app.get('/', (req, res) => {
